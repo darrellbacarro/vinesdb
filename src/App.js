0fd8fccf27598c9624a8b3999c6fd181aeafaf98
lokixxvii <darrell.bacarro@gmail.com>
2019-01-18 16:26:16 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CloseIcon from '@material-ui/icons/CancelRounded';
+
+import { Select } from 'antd';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Select
+          mode="tags"
+          style={{ width: '100%' }}
+          placeholder="Tags Mode"
+          onChange={() => {}}
+          open={false}
+          tokenSeparators={[',',';']}
+          removeIcon={<CloseIcon fontSize="small" />}
+        ></Select>
       </div>
     );
   }
